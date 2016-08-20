41af16b479dee0759a43dc2208b17ec9ee728878
David Aarons <daarons92@gmail.com>
2016-08-20 17:59:52 -0400

    Makes and uses 1 controller for navigation

--- src/main/java/com/daarons/controller/NavigationController.java
+++ src/main/java/com/daarons/controller/NavigationController.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2016 David Aarons.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.daarons.controller;
 
 import java.net.URL;
@@ -21,19 +6,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.*;
 
-/**
- * FXML Controller class
- *
- * @author David
- */
-public class HeadController implements Initializable {
-    
+public class NavigationController implements Initializable {
+
     @FXML
     private Button homeBtn;
     @FXML
@@ -68,12 +47,9 @@ public class HeadController implements Initializable {
         stage.show();
 
     }
-    /**
-     * Initializes the controller class.
-     */
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
 }
