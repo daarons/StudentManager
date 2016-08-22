165939c85878e8c132feee39723fd281179733b5
David Aarons <daarons92@gmail.com>
2016-08-22 13:01:22 -0400

    Adds import/export page

---
+++ src/main/java/com/daarons/controller/ImportExportController.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2016 David Aarons.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.daarons.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author David
+ */
+public class ImportExportController implements Initializable {
+
+    @FXML
+    private Button importBtn;
+    @FXML
+    private Button exportBtn;
+    
+    @FXML
+    private void launchFileChooser(ActionEvent event) throws Exception{
+        
+    }
+    
+    @FXML
+    private void importExport(ActionEvent event) throws Exception{
+        
+    }
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
