6b118bbd82286f215381625e629d4d4da4f40505
David Aarons <daarons92@gmail.com>
2016-09-07 17:33:03 -0400

    Adds TreeTableView to Student view
    It looks weird because of the BorderPane, but in the future I'll
    see how to make it look nicer and maybe have it slide out from
    left to right.

--- src/main/java/com/daarons/controller/StudentController.java
+++ src/main/java/com/daarons/controller/StudentController.java
@@ -18,20 +18,30 @@ package com.daarons.controller;
 import com.daarons.DAO.AccountDAO;
 import com.daarons.DAO.DAOFactory;
 import com.daarons.model.Account;
+import com.daarons.model.Session;
 import com.daarons.model.Student;
 import com.sun.javafx.scene.control.behavior.TextAreaBehavior;
 import com.sun.javafx.scene.control.skin.TextAreaSkin;
 import java.net.URL;
+import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Screen;
 
 /**
  * FXML Controller class
@@ -42,7 +52,10 @@ public class StudentController implements Initializable {
 
     private final AccountDAO dao = DAOFactory.getAccountDAO("derby");
     private Student student;
-
+    private TreeTableView sessionTableView;
+    
+    @FXML
+    private BorderPane borderPane;
     @FXML
     private TextField englishNameField;
     @FXML
@@ -59,6 +72,10 @@ public class StudentController implements Initializable {
     private TextArea notesArea;
     @FXML
     private Button saveBtn;
+    @FXML
+    private GridPane gridPaneLeft;
+    @FXML
+    private GridPane gridPaneRight;
 
     public StudentController(Student student) {
         this.student = student;
@@ -84,6 +101,10 @@ public class StudentController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        Rectangle2D screenSize = Screen.getPrimary().getVisualBounds();
+        gridPaneLeft.setMaxSize(screenSize.getWidth()/2, screenSize.getHeight()/2);
+        gridPaneRight.setMaxSize(screenSize.getWidth()/2, screenSize.getHeight()/2);
+        
         englishNameField.setText(student.getEnglishName());
         chineseNameField.setText(student.getChineseName());
         ageField.setText(String.valueOf(student.getAge()));
@@ -113,6 +134,16 @@ public class StudentController implements Initializable {
         hobbiesArea.setOnKeyPressed(handleTabAction);
         motivesArea.setOnKeyPressed(handleTabAction);
         notesArea.setOnKeyPressed(handleTabAction);
+        
+        sessionTableView = new TreeTableView();
+        sessionTableView.setMaxHeight(Double.MAX_VALUE);
+        sessionTableView.setMaxWidth(Double.MAX_VALUE);
+        TreeTableColumn<Session, String> sessionCol = new TreeTableColumn("Session");
+        TreeTableColumn<Session, Date> dateCol = new TreeTableColumn("Date");
+        sessionTableView.getColumns().addAll(sessionCol, dateCol);
+        sessionTableView.setColumnResizePolicy(TreeTableView.CONSTRAINED_RESIZE_POLICY);
+        
+        gridPaneRight.add(sessionTableView, 0, 0);
     }
 
 }
