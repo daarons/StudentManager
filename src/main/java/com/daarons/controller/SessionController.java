7e7fc42c6e840e383fd2afb301b5b1cf083b0370
David Aarons <daarons92@gmail.com>
2016-09-08 17:02:13 -0400

    Initializes session view
    Adds code for initializing the view by making sure that textareas
    can't be tabbed inside of and comboboxes have numbers for grades.
    I might remove all the FXML injections. I need to see how I'll
    save the data.

--- src/main/java/com/daarons/controller/SessionController.java
+++ src/main/java/com/daarons/controller/SessionController.java
@@ -16,9 +16,24 @@
 package com.daarons.controller;
 
 import com.daarons.model.Session;
+import com.sun.javafx.scene.control.behavior.TextAreaBehavior;
+import com.sun.javafx.scene.control.skin.TextAreaSkin;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 /**
  * FXML Controller class
@@ -32,12 +47,81 @@ public class SessionController implements Initializable {
         this.session = session;
     }
     
+    @FXML
+    private BorderPane borderPane;
+    @FXML
+    private GridPane gridPaneLeft;
+    @FXML
+    private GridPane gridPaneRight;
+    @FXML
+    private ChoiceBox fluencyCoherenceBox;
+    @FXML
+    private ChoiceBox vocabularyBox;
+    @FXML
+    private ChoiceBox grammarBox;
+    @FXML
+    private ChoiceBox pronunciationBox;
+    @FXML
+    private ChoiceBox interactEngageBox;
+    @FXML
+    private ChoiceBox commSkillsBox;
+    @FXML
+    private TextArea fluencyCoherenceReview;
+    @FXML
+    private TextArea vocabularyReview;
+    @FXML
+    private TextArea grammarReview;
+    @FXML
+    private TextArea pronunciationReview;
+    @FXML
+    private TextArea interactEngageReview;
+    @FXML
+    private TextArea commSkillsReview;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-    }    
+        ObservableList<Integer> cbList = FXCollections.observableArrayList(1, 2, 3, 4, 5);
+        ArrayList<Node> nodes = getAllNodes(borderPane);
+        for(Node node : nodes){
+            if(node instanceof ChoiceBox){
+                ((ChoiceBox)node).setItems(cbList);
+            }else if(node instanceof TextArea){
+                ((TextArea)node).setOnKeyPressed(handleTabAction);
+            }
+        }
+    }  
     
+    private static ArrayList<Node> getAllNodes(Parent root){
+        ArrayList<Node> nodes = new ArrayList();
+        addAllDescendents(root, nodes);
+        return nodes;
+    }
+    
+    private static void addAllDescendents(Parent parent, ArrayList<Node> nodes){
+        for(Node node : parent.getChildrenUnmodifiable()){
+            nodes.add(node);
+            if(node instanceof Parent){
+                addAllDescendents((Parent)node, nodes);
+            }
+        }
+    }
+    
+    EventHandler<KeyEvent> handleTabAction = new EventHandler<KeyEvent>() {
+        @Override
+        public void handle(KeyEvent event) {
+            if (event.getCode() == KeyCode.TAB) {
+                TextArea text = (TextArea) event.getSource();
+                TextAreaSkin skin = (TextAreaSkin) text.getSkin();
+                if (skin.getBehavior() instanceof TextAreaBehavior) {
+                    TextAreaBehavior behavior = (TextAreaBehavior) skin.getBehavior();
+                    behavior.callAction("TraverseNext");
+                    event.consume();
+                }
+            }
+        }
+    };
+
 }
