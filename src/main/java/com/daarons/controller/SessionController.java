bbeb43d1cd9e102739c0cc52f3a2dda6355a39af
David Aarons <daarons92@gmail.com>
2016-09-09 15:40:37 -0400

    Adds save behavoir, initializes data, and gets nodes

--- src/main/java/com/daarons/controller/SessionController.java
+++ src/main/java/com/daarons/controller/SessionController.java
@@ -15,7 +15,14 @@
  */
 package com.daarons.controller;
 
+import com.daarons.DAO.AccountDAO;
+import com.daarons.DAO.DAOFactory;
+import com.daarons.model.Account;
+import com.daarons.model.Note;
+import com.daarons.model.Review;
+import com.daarons.model.ReviewSection;
 import com.daarons.model.Session;
+import com.daarons.model.Student;
 import com.sun.javafx.scene.control.behavior.TextAreaBehavior;
 import com.sun.javafx.scene.control.skin.TextAreaSkin;
 import java.net.URL;
@@ -23,11 +30,13 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyCode;
@@ -41,18 +50,33 @@ import javafx.scene.layout.GridPane;
  * @author David
  */
 public class SessionController implements Initializable {
+
+    private final AccountDAO dao = DAOFactory.getAccountDAO("derby");
     private Session session;
-    
-    public SessionController(Session session){
+    private ArrayList<Node> nodes;
+
+    public SessionController(Session session) {
         this.session = session;
     }
-    
+
     @FXML
     private BorderPane borderPane;
     @FXML
     private GridPane gridPaneLeft;
     @FXML
     private GridPane gridPaneRight;
+    @FXML
+    private TextArea fluencyCoherenceNotes;
+    @FXML
+    private TextArea vocabularyNotes;
+    @FXML
+    private TextArea grammarNotes;
+    @FXML
+    private TextArea pronunciationNotes;
+    @FXML
+    private TextArea interactEngageNotes;
+    @FXML
+    private TextArea commSkillsNotes;
     @FXML
     private ChoiceBox fluencyCoherenceBox;
     @FXML
@@ -77,7 +101,9 @@ public class SessionController implements Initializable {
     private TextArea interactEngageReview;
     @FXML
     private TextArea commSkillsReview;
-    
+    @FXML
+    private Button saveBtn;
+
     /**
      * Initializes the controller class.
      */
@@ -85,30 +111,106 @@ public class SessionController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         ObservableList<Integer> cbList = FXCollections.observableArrayList(1, 2, 3, 4, 5);
         ArrayList<Node> nodes = getAllNodes(borderPane);
-        for(Node node : nodes){
-            if(node instanceof ChoiceBox){
-                ((ChoiceBox)node).setItems(cbList);
-            }else if(node instanceof TextArea){
-                ((TextArea)node).setOnKeyPressed(handleTabAction);
+        this.nodes = nodes;
+        for (Node node : nodes) {
+            if (node instanceof ChoiceBox) {
+                ((ChoiceBox) node).setItems(cbList);
+                ((ChoiceBox) node).getSelectionModel().selectFirst();
+            } else if (node instanceof TextArea) {
+                ((TextArea) node).setOnKeyPressed(handleTabAction);
             }
         }
-    }  
-    
-    private static ArrayList<Node> getAllNodes(Parent root){
+
+        Note sessionNotes = session.getNotes();
+        if (sessionNotes != null) {
+            fluencyCoherenceNotes.setText(sessionNotes.getFluencyAndCoherence());
+            vocabularyNotes.setText(sessionNotes.getVocabulary());
+            grammarNotes.setText(sessionNotes.getGrammar());
+            pronunciationNotes.setText(sessionNotes.getPronunciation());
+            interactEngageNotes.setText(sessionNotes.getInteractionAndEngagement());
+            commSkillsNotes.setText(sessionNotes.getCommunicationSkills());
+        }
+
+        Review sessionReview = session.getReview();
+        if (sessionReview != null) {
+            fluencyCoherenceBox.setValue(sessionReview.getFluencyAndCoherence().getGrade());
+            fluencyCoherenceReview.setText(sessionReview.getFluencyAndCoherence().getComment());
+            vocabularyBox.setValue(sessionReview.getVocabulary().getGrade());
+            vocabularyReview.setText(sessionReview.getVocabulary().getComment());
+            grammarBox.setValue(sessionReview.getGrammar().getGrade());
+            grammarReview.setText(sessionReview.getGrammar().getComment());
+            pronunciationBox.setValue(sessionReview.getPronunciation().getGrade());
+            pronunciationReview.setText(sessionReview.getPronunciation().getComment());
+            interactEngageBox.setValue(sessionReview.getInteractionAndEngagement().getGrade());
+            interactEngageReview.setText(sessionReview.getInteractionAndEngagement().getComment());
+            commSkillsBox.setValue(sessionReview.getCommunicationSkills().getGrade());
+            commSkillsReview.setText(sessionReview.getCommunicationSkills().getComment());
+        }
+
+        saveBtn.setOnAction((ActionEvent event) -> {
+            Note notes = new Note();
+            notes.setFluencyAndCoherence(fluencyCoherenceNotes.getText());
+            notes.setVocabulary(vocabularyNotes.getText());
+            notes.setGrammar(grammarNotes.getText());
+            notes.setPronunciation(pronunciationNotes.getText());
+            notes.setInteractionAndEngagement(interactEngageNotes.getText());
+            notes.setCommunicationSkills(commSkillsNotes.getText());
+            notes.setSession(session);
+            session.setNotes(notes);
+
+            Review review = new Review();
+            review.setFluencyAndCoherence(
+                    new ReviewSection((int) fluencyCoherenceBox.getValue(),
+                            fluencyCoherenceReview.getText()));
+            review.setVocabulary(
+                    new ReviewSection((int) vocabularyBox.getValue(),
+                            vocabularyReview.getText()));
+            review.setGrammar(
+                    new ReviewSection((int) grammarBox.getValue(),
+                            grammarReview.getText()));
+            review.setPronunciation(
+                    new ReviewSection((int) pronunciationBox.getValue(),
+                            pronunciationReview.getText()));
+            review.setInteractionAndEngagement(
+                    new ReviewSection((int) interactEngageBox.getValue(),
+                            interactEngageReview.getText()));
+            review.setCommunicationSkills(
+                    new ReviewSection((int) commSkillsBox.getValue(),
+                            commSkillsReview.getText()));
+            review.setSession(session);
+            session.setReview(review);
+            
+            Account account = session.getStudent().getAccount();
+            for (Student student : account.getStudents()) {
+                if (student.getId() == session.getStudent().getId()) {
+                    for (Session s : student.getSessions()) {
+                        if (s.getId() == session.getId()) {
+                            s = session;
+                            break;
+                        }
+                    }
+                }
+            }
+            dao.updateAccount(account);
+        });
+
+    }
+
+    private static ArrayList<Node> getAllNodes(Parent root) {
         ArrayList<Node> nodes = new ArrayList();
         addAllDescendents(root, nodes);
         return nodes;
     }
-    
-    private static void addAllDescendents(Parent parent, ArrayList<Node> nodes){
-        for(Node node : parent.getChildrenUnmodifiable()){
+
+    private static void addAllDescendents(Parent parent, ArrayList<Node> nodes) {
+        for (Node node : parent.getChildrenUnmodifiable()) {
             nodes.add(node);
-            if(node instanceof Parent){
-                addAllDescendents((Parent)node, nodes);
+            if (node instanceof Parent) {
+                addAllDescendents((Parent) node, nodes);
             }
         }
     }
-    
+
     EventHandler<KeyEvent> handleTabAction = new EventHandler<KeyEvent>() {
         @Override
         public void handle(KeyEvent event) {
