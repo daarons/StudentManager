5c730c96389d4e89890860ad621cb9078bbfd4fe
David Aarons <daarons92@gmail.com>
2016-09-15 17:39:01 -0400

    Adds 2 textfields for picking account/student

--- src/main/java/com/daarons/controller/NotesController.java
+++ src/main/java/com/daarons/controller/NotesController.java
@@ -17,22 +17,26 @@ package com.daarons.controller;
 
 import com.daarons.DAO.AccountDAO;
 import com.daarons.DAO.DAOFactory;
+import com.daarons.model.Account;
+import com.daarons.model.Student;
 import com.sun.javafx.scene.control.behavior.TextAreaBehavior;
 import com.sun.javafx.scene.control.skin.TextAreaSkin;
 import java.net.URL;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Screen;
 import jfxtras.scene.control.CalendarTimePicker;
+import org.controlsfx.control.textfield.TextFields;
 
 /**
  * FXML Controller class
@@ -40,7 +44,10 @@ import jfxtras.scene.control.CalendarTimePicker;
  * @author David
  */
 public class NotesController implements Initializable {
+
     private final AccountDAO dao = DAOFactory.getAccountDAO("derby");
+    private Account account;
+    private Student student;
 
     @FXML
     private TilePane tilePane;
@@ -60,27 +67,31 @@ public class NotesController implements Initializable {
     private Button saveBtn;
     @FXML
     private CalendarTimePicker timePicker;
-    
     @FXML
-    private void handleTabAction(KeyEvent event) throws Exception{
-        if(event.getCode()==KeyCode.TAB){
+    private TextField accountField;
+    @FXML
+    private TextField studentField;
+
+    @FXML
+    private void handleTabAction(KeyEvent event) throws Exception {
+        if (event.getCode() == KeyCode.TAB) {
             TextArea text = (TextArea) event.getSource();
             TextAreaSkin skin = (TextAreaSkin) text.getSkin();
-            if(skin.getBehavior() instanceof TextAreaBehavior){
+            if (skin.getBehavior() instanceof TextAreaBehavior) {
                 TextAreaBehavior behavior = (TextAreaBehavior) skin.getBehavior();
                 behavior.callAction("TraverseNext");
                 event.consume();
             }
         }
     }
-    
+
     @FXML
-    private void saveNotes(KeyEvent event) throws Exception{
-        if(event.getSource() == saveBtn){
+    private void saveNotes(KeyEvent event) throws Exception {
+        if (event.getSource() == saveBtn) {
             //grab account and update
         }
     }
-    
+
     /**
      * Initializes the controller class.
      */
@@ -89,19 +100,27 @@ public class NotesController implements Initializable {
         Rectangle2D screenSize = Screen.getPrimary().getVisualBounds();
         tilePane.setPrefTileHeight(screenSize.getHeight() / 4.5);
         tilePane.setPrefTileWidth(screenSize.getWidth() / 6.5);
-        
+
         timePicker.setPrefSize(screenSize.getWidth() / 5, 60);
         Calendar cal = Calendar.getInstance();
         cal.setTimeInMillis(System.currentTimeMillis());
         int minute = cal.get(Calendar.MINUTE);
         int hour = cal.get(Calendar.HOUR_OF_DAY);
-        if(minute > 5 && minute < 35){
+        if (minute > 5 && minute < 35) {
             cal.set(Calendar.MINUTE, 30);
-        }else{
+        } else {
             cal.set(Calendar.MINUTE, 00);
             cal.set(Calendar.HOUR_OF_DAY, hour + 1);
         }
         timePicker.setCalendar(cal);
-    }    
-    
+
+        TextFields.bindAutoCompletion(accountField, t -> {
+            return dao.getAccounts(t.getUserText());
+        });
+
+        studentField.disableProperty().bind(
+                Bindings.isEmpty(accountField.textProperty()));
+
+    }
+
 }
