0c535c98f73bf643cf2e39cb33a8f27f5e989f94
David Aarons <daarons92@gmail.com>
2016-09-29 15:54:50 -0400

    Removes unneeded import

--- src/main/java/com/daarons/studentmanager/StudentManagerApp.java
+++ src/main/java/com/daarons/studentmanager/StudentManagerApp.java
@@ -1,7 +1,6 @@
 package com.daarons.studentmanager;
 
 import com.daarons.DAO.EMSingleton;
-import java.io.*;
 import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
